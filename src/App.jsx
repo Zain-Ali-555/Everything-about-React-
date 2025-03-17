98a4cf03f02b381c13dae41db2907a0fae176879
Zain Ali <zainchamp5@gmail.com>
2025-03-17 08:33:47 +0500

    Exercice 5 Done about Conditional rendering.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import Product from "./components/Product";
-import Person from "./components/Person";
+import React from "react";
 
 const App = () => {
+  const temperature = (temp) => {
+    if (temp < 15) return <h1>Its cold outside.</h1>;
+    if (temp >= 15 && temp <= 25) return <h1>Its nice outside.</h1>;
+    if (temp > 25) return <h1>Its hot outside.</h1>;
+  };
+
+  const userStatus = (isAdmin, LoggedIn) => {
+    if (isAdmin == true && LoggedIn == true) return <h1>Welcome Admin.</h1>;
+    if (isAdmin == false && LoggedIn == true) return <h1>Welcome User.</h1>;
+  };
+
   return (
     <>
       <div>
-        <Person name="MalikZain555" age={19} />
-        <Product name="Phone" price={2000} />
+        <h1>{userStatus(true, true)}</h1>
+        <h5>{temperature(15)}</h5>
       </div>
     </>
   );
---
+++ src/components/UserStatus.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const UserStatus = () => {
+  
+  return <div></div>;
+};
+
+export default UserStatus;
